6e6b6e1fd80d1fe8331ae6646e915f7ea92ccd07
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-03-23 17:40:43 +0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UserModule } from './user/user.module';
     }),
     AuthModule,
     UserModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,7 +22,7 @@ export class AuthController {
 
     res.cookie('access_token', result.access_token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
