303642a77cf3fafddf3b142b89718f54256f1a25
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-03-23 17:34:08 +0400

    fix:samesite:lax

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,7 +23,7 @@ export class AuthController {
     res.cookie('access_token', result.access_token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
